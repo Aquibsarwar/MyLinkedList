d36db968303f480eb82123727381be50f1700c36
Aquibsarwar <saquib@sun21.co.jp>
2019-10-18 15:52:42 +0900

    added delete and addlastt

--- src/main/java/LinkedList/LinkedList/LinkListPractice.java
+++ src/main/java/LinkedList/LinkedList/LinkListPractice.java
@@ -23,7 +23,9 @@ public class LinkListPractice
 		}
 	 	
 	}
-	public void show() {
+	
+	public void show() 
+	{
 		Node node=head;
 		while(node.next!=null) {
 			System.out.println(node.data);
@@ -33,20 +35,80 @@ public class LinkListPractice
 	}
 
 
-	public void insertAtFirst(int data) {
+	public void insertAtFirst(int data) 
+	{
 		
 		Node node = new Node();
 		node.data=data;
 		node.next=head;
 		head = node;
+
+	}
+
+	public void insertAtanyindex(int index, int data) 
+	{
+	
+		Node node = new Node();
+		node.data = data;
 		
+		Node n = head;
+		if(index ==0) {
+			insertAtFirst(data);
+		}
+		else
+		{
+			for(int firstElement = 0; firstElement<index-1; firstElement++) {
+				n = n.next;
+			}
 		
+		node.next=n.next;
+		n.next=node;
+		}
+	}
+
+	
+	public void deleteList(int index) 
+	{
+		Node node = new Node();
+		Node n=head;
 		
+		if(index ==0) {
+			head=n.next;
+		}
+		else 
+		{
+			for(int i = 0; i<index-1;i++) 
+			{
+				n=n.next;
+			}
 		
+			Node n2 = n.next;
+			node = n;
+			node.next = n2.next;
+			n2=null;
+		}
+	}
+	
+	public void addlast(int data) {
+		
+		Node node = new Node();
+		Node n = head;
+		
+		while(n.next!=null) {
+			n = n.next;
+			//System.out.print("here!");
+			if(n.next==null) {
+				break;
+			}
+		}
+		node = n;
+		node.data = data;	
 		
 	}
 
-
-
-
 }
+
+
+
+
+
--- src/main/java/LinkedList/LinkedList/LinkMain.java
+++ src/main/java/LinkedList/LinkedList/LinkMain.java
@@ -8,10 +8,27 @@ public class LinkMain {
 		linklist.insert(1);
 		linklist.insert(2);
 		linklist.insert(3);
-		linklist.insertAtFirst(11);
+		linklist.insert(4);
+		linklist.insert(1);
+		linklist.insert(2);
+		linklist.insert(3);
+		linklist.insert(4);
+		linklist.insert(5);
+		
+		
+		
+		
+		//linklist.insertAtFirst(11);
+		//linklist.insertAtanyindex(2,45);
+		//linklist.insertAtanyindex(3,46);
+		//linklist.insertAtanyindex(0,0);
+		
+		linklist.deleteList(8);
+		linklist.addlast(99);
 		
 		
 		linklist.show();
+		
 		
 	}
 	
